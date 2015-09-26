a26b40a07cd4bf49eb93ca6434a318e131ea9234
Erik Høj <erikhoj.p@gmail.com>
2015-09-26 02:09:31 +0200

    add directionToVector function

--- Rabbit.java
+++ Rabbit.java
@@ -5,6 +5,11 @@
  * @author Morten D. Bech
  * @version September 8, 2015
  */
+
+import java.awt.Point;
+import java.lang.Math;
+import java.util.ArrayList;
+
 public class Rabbit extends Animal {
     /**
      * In order to create a new Rabbit we need to provide a
@@ -15,11 +20,24 @@ public class Rabbit extends Animal {
     
     private boolean moveStraight;
     private Direction result;
+    private ArrayList<Point> foxes;
+    private ArrayList<Point> bushes;
+    private ArrayList<Point> carrots;
+    private ArrayList<Point> edges;
+    
+    private ArrayList<Integer> foxesTimer;
     
     public Rabbit(Model model, Position position) {
         super(model, position);
         moveStraight = false;
         result = Direction.STAY;
+        
+        foxes = new ArrayList<Point>();
+        bushes = new ArrayList<Point>();
+        carrots = new ArrayList<Point>();
+        edges = new ArrayList<Point>();
+        
+        foxesTimer = new ArrayList<Integer>();
     }
     
     /**
@@ -27,11 +45,48 @@ public class Rabbit extends Animal {
      */
     @Override
     
-    public Direction decideDirection() {
+    public Direction decideDirection() {       
         return result;
     }
     
-    public Direction decideDirection2() {
+    private void getAngle(Direction d, Point oPos) {
+        
+    }
+    
+    private Point directionToVector(Direction d) {
+        if(d == Direction.STAY) {
+            return null;
+        }
+        else if(d == Direction.N) {
+            return new Point(0,1);
+        }
+        else if(d == Direction.NE) {
+            return new Point(1,1);
+        }
+        else if(d == Direction.E) {
+            return new Point(1,0);
+        }
+        else if(d == Direction.SE) {
+            return new Point(1,-1);
+        }
+        else if(d == Direction.S) {
+            return new Point(0,-1);
+        }
+        else if(d == Direction.SW) {
+            return new Point(-1,-1);
+        }
+        else if(d == Direction.W) {
+            return new Point(-1,0);
+        }
+        else if(d == Direction.NW) {
+            return new Point(-1,1);
+        }
+        
+        return null;
+        }
+    
+    
+    private Direction decideDirection2() {
         for(Direction d : Direction.allDirections()) {
             Class<?> c = look(d);
             
@@ -53,7 +108,7 @@ public class Rabbit extends Animal {
         return result;
     }
     
-    public Direction decideDirection1() {
+    private Direction decideDirection1() {
         for(Direction d : Direction.allDirections()) {
             Class<?> c = look(d);
             
@@ -69,7 +124,7 @@ public class Rabbit extends Animal {
         return result;
     }
     
-    public Boolean isOccupied(Direction d) {
+    private Boolean isOccupied(Direction d) {
         boolean result = false;
         
         if(distance(d) == 1) {
